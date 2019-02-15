2de5927fba9ccc8c0fb8f11abbdf88dcc8f02364
matthewjmontalto <matthewjmontalto@gmail.com>
2019-02-15 14:14:29 -0500

    Clean Up Console.log
    - remove console.log from win checks

--- assets/scripts/game/game-engine.js
+++ assets/scripts/game/game-engine.js
@@ -26,47 +26,55 @@ const turnCounter = (playerChoice) => {
 }
 
 const checkForWinner = () => {
-  console.log(gameBoard)
+  let gameOutcome = ''
   if (
     (gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
     (gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
     (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') ||
     (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
     (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
     (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
     (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
     (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (
     (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') ||
     (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')
   ) {
-    return console.log('Winner!')
+    gameOutcome = 'Winner'
+    return gameOutcome
   } else if (turn === 9) {
     console.log('Tie!')
   } else {
